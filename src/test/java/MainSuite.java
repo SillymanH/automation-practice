569bc359c65a040f8efc738b17543a71e7514829
SillymanH <sleiman19494@gmail.com>
2021-05-25 06:32:29 +0300

    Commit Project Files

---
+++ src/test/java/MainSuite.java
@@ -0,0 +1,182 @@
+import dataProvider.ConfigFileReader;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.runners.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MainSuite {
+
+    private static ChromeDriver driver;
+    private static WebDriverWait wait;
+    private WebElement iframe;
+    private String EXPECTED_RESULT_SUCCESS_LABEL;
+    private String ACTUAL_RESULT_LABEL;
+    private static ConfigFileReader configFileReader;
+
+    @BeforeClass
+    public static void init(){
+
+        configFileReader = new ConfigFileReader();
+        System.setProperty("webdriver.chrome.driver", configFileReader.getDriverPath());
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void AgoToHomePage(){
+
+        String URL = configFileReader.getURL();
+        String EXPECTED_RESULT_SUCCESS_LABEL = "My Store";
+
+        driver.get(URL);
+        //TODO: Read URL from external file or properties
+        String ACTUAL_RESULT_LABEL = driver.getTitle();
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void BselectProduct(){
+
+        String itemURL = "http://automationpractice.com/index.php?id_product=2&controller=product";
+        String EXPECTED_RESULT_SUCCESS_LABEL = "Blouse";
+        String ACTUAL_RESULT_LABEL = "";
+
+        List<WebElement> aTagElements = driver.findElementsByTagName("a");;
+        for (WebElement quickViewElement : aTagElements){
+            if (quickViewElement.getAttribute("href").equals(itemURL)) {
+
+                quickViewElement.click();
+                wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("fancybox-iframe")));
+                iframe = driver.findElementByClassName("fancybox-iframe");
+                driver.switchTo().frame(iframe);
+                List<WebElement> headerElements = driver.findElementsByTagName("h1");
+                for (WebElement header : headerElements) {
+
+                    ACTUAL_RESULT_LABEL = header.getAttribute("innerHTML");
+                    break;
+                }
+                break;
+            }
+        }
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void CaddToCart(){
+
+        String EXPECTED_RESULT_SUCCESS_LABEL = "Product successfully added to your shopping cart";
+        String ACTUAL_RESULT_LABEL = "";
+
+        WebElement addToCartBtn = driver.findElementByName("Submit");
+        addToCartBtn.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("icon-ok")));
+        List<WebElement> addToCartHeaders = driver.findElementsByTagName("h2");
+        for (WebElement header : addToCartHeaders) {
+            if (header.getAttribute("innerText").equals(EXPECTED_RESULT_SUCCESS_LABEL)) {
+
+                ACTUAL_RESULT_LABEL = header.getAttribute("innerText");
+                break;
+            }
+        }
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void DproceedToCheckout() {
+
+        EXPECTED_RESULT_SUCCESS_LABEL = "Your shopping cart";
+
+        WebElement proceedToCheckoutButton = driver.findElementByClassName("button-medium");
+        proceedToCheckoutButton.click();
+        ACTUAL_RESULT_LABEL = driver.findElementByClassName("navigation_page").getAttribute("innerText");
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void EvalidateSummary() {
+
+        EXPECTED_RESULT_SUCCESS_LABEL = "Authentication";
+
+        WebElement proceedToCheckoutButton = driver.findElementByClassName("standard-checkout");
+        proceedToCheckoutButton.click();
+        ACTUAL_RESULT_LABEL = driver.findElementByClassName("navigation_page").getAttribute("innerText");
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void FsignIn() {
+
+        EXPECTED_RESULT_SUCCESS_LABEL = "Addresses";
+        String USERNAME = configFileReader.getUsername();
+        String PASSWORD = configFileReader.getPassword();
+
+        WebElement emailField = driver.findElementById("email");
+        WebElement passwordField = driver.findElementById("passwd");
+        WebElement submitButton = driver.findElementById("SubmitLogin");
+        emailField.sendKeys(USERNAME);
+        passwordField.sendKeys(PASSWORD);
+        submitButton.click();
+        ACTUAL_RESULT_LABEL = driver.findElementByClassName("navigation_page").getAttribute("innerText");
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void GvalidateShippingAddress() {
+
+        EXPECTED_RESULT_SUCCESS_LABEL = "Shipping";
+
+        WebElement proceedToCheckoutButton = driver.findElementByName("processAddress");
+        proceedToCheckoutButton.click();
+        ACTUAL_RESULT_LABEL = driver.findElementByClassName("navigation_page").getAttribute("innerText");
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void HchooseShippingMethod() {
+
+        EXPECTED_RESULT_SUCCESS_LABEL = "Your payment method";
+
+        WebElement checkbox = driver.findElementById("cgv");
+        WebElement proceedToCheckoutButton = driver.findElementByName("processCarrier");
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+        }
+        proceedToCheckoutButton.click();
+        ACTUAL_RESULT_LABEL = driver.findElementByClassName("navigation_page").getAttribute("innerText");
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void IchoosePaymentMethod() {
+
+        EXPECTED_RESULT_SUCCESS_LABEL = "Bank-wire payment.";
+        WebElement payByBankWireButton = driver.findElementByClassName("bankwire");
+        payByBankWireButton.click();
+        ACTUAL_RESULT_LABEL = driver.findElementByClassName("navigation_page").getAttribute("innerText");
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @Test
+    public void JconfirmOrder() {
+
+        EXPECTED_RESULT_SUCCESS_LABEL = "Order confirmation";
+
+        WebElement confirmOrderButton = driver.findElementByCssSelector("#cart_navigation > button");
+        confirmOrderButton.click();
+        ACTUAL_RESULT_LABEL = driver.findElementByClassName("navigation_page").getAttribute("innerText");
+        Assert.assertEquals(EXPECTED_RESULT_SUCCESS_LABEL, ACTUAL_RESULT_LABEL);
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.close();
+    }
+
+}
---
+++ src/test/java/dataProvider/ConfigFileReader.java
@@ -0,0 +1,73 @@
+package dataProvider;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Properties;
+
+public class ConfigFileReader {
+
+    private Properties properties;
+    private final String propertyFilePath= "Configs//Configuration.properties";
+
+    public ConfigFileReader(){
+
+        BufferedReader reader;
+
+        try {
+            reader = new BufferedReader(new FileReader(propertyFilePath));
+            properties = new Properties();
+            try {
+                properties.load(reader);
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Configuration.properties not found at " + propertyFilePath);
+        }
+    }
+
+    public String getUsername() {
+
+        String username = properties.getProperty("username");
+        if(username!= null)
+            return username;
+        else throw new RuntimeException("username not specified in the Configuration.properties file.");
+    }
+
+    public String getPassword() {
+
+        String decodedPassword = decodePassword(properties.getProperty("password"));
+        if(decodedPassword!= null)
+            return decodedPassword;
+        else throw new RuntimeException("password is not specified in the Configuration.properties file.");
+    }
+
+    public String decodePassword(String encodedBytes) {
+
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedBytes);
+        return new String(decodedBytes);
+    }
+
+    public String getURL() {
+
+        String URL = properties.getProperty("url");
+        if(URL!= null)
+            return URL;
+        else throw new RuntimeException("URL is not specified in the Configuration.properties file.");
+    }
+
+    public String getDriverPath() {
+
+        String driverPath = properties.getProperty("driverPath");
+        if(driverPath!= null)
+            return driverPath;
+        else throw new RuntimeException("driverPath not specified in the Configuration.properties file.");
+    }
+
+}
